585d74baaa35edc1fae5b7f62c6ced49e8715003
Caleb Joshua <calebjoshuapaul@gmail.com>
2024-06-29 17:01:41 +0530

    refactor: code cleanup

--- app/components/VirtualCanvas.tsx
+++ app/components/VirtualCanvas.tsx
@@ -1,14 +1,136 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
-import { GestureRecognizer, HandLandmarker } from "@mediapipe/tasks-vision";
+import { Suspense, useEffect, useRef, useState } from "react";
+import {
+	DrawingUtils,
+	GestureRecognizer,
+	GestureRecognizerResult,
+	NormalizedLandmark,
+} from "@mediapipe/tasks-vision";
 
 import WebCam from "./WebCam";
 
-export default function VirtualCanvas({ vision }: { vision: any }) {
-	const resCanvas = useRef<HTMLCanvasElement>(null);
+const lastVideoTimeM = 0;
+
+interface WasmFileset {
+	/** The path to the Wasm loader script. */
+	wasmLoaderPath: string;
+	/** The path to the Wasm binary. */
+	wasmBinaryPath: string;
+	/** The optional path to the asset loader script. */
+	assetLoaderPath?: string;
+	/** The optional path to the assets binary. */
+	assetBinaryPath?: string;
+}
+function drawHandsOnVideo(
+	ctx: CanvasRenderingContext2D,
+	canvasElement: HTMLCanvasElement,
+	landmarks: NormalizedLandmark[][]
+) {
+	const drawingUtils = new DrawingUtils(ctx);
+
+	ctx.save();
+	ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+	ctx.scale(1, 1);
+
+	for (const landmark of landmarks) {
+		drawingUtils.drawConnectors(landmark, GestureRecognizer.HAND_CONNECTIONS, {
+			color: "#00FF00",
+			lineWidth: 1.5,
+		});
+		drawingUtils.drawLandmarks(landmark, {
+			color: "#FF0000",
+			lineWidth: 1,
+			radius: 1,
+		});
+	}
+
+	ctx.restore();
+}
+
+function drawResultsOnCanvas(
+	ctx: CanvasRenderingContext2D,
+	canvasElement: HTMLCanvasElement,
+	results: GestureRecognizerResult
+) {
+	if (results.gestures.length < 1) return;
+	const categoryName = results.gestures[0][0].categoryName;
+	const categoryScore = (results.gestures[0][0].score * 100).toFixed(2);
+	const handedness = results.handedness[0][0].displayName;
+
+	ctx.save();
+	ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+	ctx.scale(1, 1);
+
+	ctx.fillText(`GestureRecognizer: ${categoryName}`, 10, 20);
+	ctx.fillText(`Confidence: ${categoryScore}`, 10, 40);
+	ctx.fillText(`Handedness: ${handedness}`, 10, 60);
+
+	ctx.restore();
+}
+
+const predictWebcam = (
+	gestureRecognizer: GestureRecognizer,
+	videoElement: HTMLVideoElement,
+	canvasElement: HTMLCanvasElement,
+	resCanvasElement: HTMLCanvasElement,
+	lastVideoTime: number = lastVideoTimeM
+) => {
+	const canvasCtx = canvasElement?.getContext("2d");
+	if (!canvasCtx) return;
+
+	const resCanvasCtx = resCanvasElement.getContext("2d");
+	if (!resCanvasCtx) return;
+
+	let startTimeMs = Date.now();
+
+	const results = gestureRecognizer.recognizeForVideo(
+		videoElement,
+		startTimeMs
+	);
+
+	if (results && results?.landmarks) {
+		drawHandsOnVideo(canvasCtx, canvasElement, results.landmarks);
+		drawResultsOnCanvas(resCanvasCtx, resCanvasElement, results);
+	}
+
+	window.requestAnimationFrame(() =>
+		predictWebcam(
+			gestureRecognizer,
+			videoElement,
+			canvasElement,
+			resCanvasElement,
+			lastVideoTime
+		)
+	);
+};
+
+export default function VirtualCanvas({ vision }: { vision: WasmFileset }) {
+	const camRef = useRef<HTMLVideoElement>(null);
+
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const resCanvasRef = useRef<HTMLCanvasElement>(null);
+
 	const [gestureRecognizer, setGestureRecognizer] =
 		useState<GestureRecognizer>();
+	const [isCamMounted, setIsCamMounted] = useState(false);
+
+	useEffect(() => {
+		if (
+			camRef.current &&
+			canvasRef.current &&
+			resCanvasRef.current &&
+			isCamMounted &&
+			gestureRecognizer
+		) {
+			predictWebcam(
+				gestureRecognizer,
+				camRef.current,
+				canvasRef.current,
+				resCanvasRef.current
+			);
+		}
+	}, [isCamMounted, gestureRecognizer]);
 
 	useEffect(() => {
 		(async () => {
@@ -25,18 +147,50 @@ export default function VirtualCanvas({ vision }: { vision: any }) {
 		})();
 	}, [vision]);
 
+	useEffect(() => {
+		if (camRef) {
+			navigator.mediaDevices
+				.getUserMedia({ video: { facingMode: "user" } })
+				.then((stream) => {
+					if (camRef.current) {
+						camRef.current.srcObject = stream;
+						camRef.current.onloadedmetadata = () => {
+							setIsCamMounted(true);
+							camRef.current?.play();
+							//Start gesture predict function
+						};
+					}
+				})
+				.catch((e) => {
+					console.log(e);
+				});
+		}
+	}, [camRef]);
+
+	if (gestureRecognizer === undefined) {
+		return <div>Loading...</div>;
+	}
+
 	return (
 		<div className="flex gap-2 flex-wrap">
-			<canvas
-				ref={resCanvas}
-				className="border-2 border-gray-100 flex-1 h-96 w-[450px] max-w-[450px] bg-gray-50"
-			/>
-			{gestureRecognizer && (
-				<WebCam
-					resCanvas={resCanvas}
-					gestureRecognizer={gestureRecognizer}
+			<Suspense
+				fallback={<div>Loading...</div>}
+				key={"resCanvas"}
+			>
+				<canvas
+					ref={resCanvasRef}
+					className="border-2 border-gray-100 flex-1 h-96 w-[450px] max-w-[450px] bg-gray-50"
 				/>
-			)}
+			</Suspense>
+			<Suspense
+				fallback={<div>Loading...</div>}
+				key={"webCam"}
+			>
+				<WebCam
+					camRef={camRef}
+					canvasRef={canvasRef}
+				/>
+			</Suspense>
 		</div>
 	);
 }
--- app/components/WebCam.tsx
+++ app/components/WebCam.tsx
@@ -1,134 +1,13 @@
 "use client";
-import { DrawingUtils, GestureRecognizer } from "@mediapipe/tasks-vision";
-import React, { useEffect, useRef, RefObject, useState } from "react";
-
-const videoConstraints = {
-	facingMode: "user",
-	video: true,
-};
-
-async function predictWebcam(
-	videoElement: HTMLVideoElement,
-	canvasElement: HTMLCanvasElement,
-	lastVideoTime: number,
-	gestureRecognizer: GestureRecognizer,
-	setLastVideoTime: (time: number) => void,
-	resCanvas: RefObject<HTMLCanvasElement>
-) {
-	const height = 384;
-	const width = 450;
-
-	const canvasCtx = canvasElement.getContext("2d");
-	if (!canvasCtx) return;
-	const drawingUtils = new DrawingUtils(canvasCtx);
-
-	let results;
-	let startTimeMs = Date.now();
-
-	if (Math.round(lastVideoTime) !== Math.round(videoElement.currentTime)) {
-		setLastVideoTime(Math.round(videoElement.currentTime));
-		results = gestureRecognizer.recognizeForVideo(videoElement, startTimeMs);
-	}
-	if (results?.landmarks) {
-		canvasCtx.save();
-		canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-		canvasCtx.scale(1, 1);
-
-		for (const landmarks of results.landmarks) {
-			drawingUtils.drawConnectors(
-				landmarks,
-				GestureRecognizer.HAND_CONNECTIONS,
-				{
-					color: "#00FF00",
-					lineWidth: 1.5,
-				}
-			);
-			drawingUtils.drawLandmarks(landmarks, {
-				color: "#FF0000",
-				lineWidth: 1,
-				radius: 1,
-			});
-		}
-		const resCanvasElement = resCanvas.current;
-		if (results.gestures.length > 0 && resCanvasElement) {
-			resCanvasElement.height = height;
-			resCanvasElement.width = width;
-			const resCanvasCtx = resCanvasElement.getContext("2d");
-			if (!resCanvasCtx) return;
-			const categoryName = results.gestures[0][0].categoryName;
-			const categoryScore = (results.gestures[0][0].score * 100).toFixed(2);
-			const handedness = results.handednesses[0][0].displayName;
-
-			resCanvasCtx.save();
-			resCanvasCtx.clearRect(
-				0,
-				0,
-				resCanvasElement.width,
-				resCanvasElement.height
-			);
-			resCanvasCtx.scale(2, 2);
-			resCanvasCtx.fillText(`GestureRecognizer: ${categoryName}`, 10, 20);
-			resCanvasCtx.fillText(`Confidence: ${categoryScore}`, 10, 40);
-			resCanvasCtx.fillText(`Handedness: ${handedness}`, 10, 60);
-			resCanvasCtx.restore();
-		}
-
-		canvasCtx.restore();
-	}
-
-	window.requestAnimationFrame(() =>
-		predictWebcam(
-			videoElement,
-			canvasElement,
-			lastVideoTime,
-			gestureRecognizer,
-			setLastVideoTime,
-			resCanvas
-		)
-	);
-}
+import React, { RefObject } from "react";
 
 export default function WebCam({
-	gestureRecognizer,
-	resCanvas,
+	camRef,
+	canvasRef,
 }: {
-	gestureRecognizer: GestureRecognizer;
-	resCanvas: RefObject<HTMLCanvasElement>;
+	camRef: RefObject<HTMLVideoElement>;
+	canvasRef: RefObject<HTMLCanvasElement>;
 }) {
-	const camRef = useRef<HTMLVideoElement>(null);
-	const canvasRef = useRef<HTMLCanvasElement>(null);
-	const [lastVideoTime, setLastVideoTime] = useState(0);
-
-	useEffect(() => {
-		const startPrediction = async () => {
-			if (camRef.current && canvasRef.current) {
-				predictWebcam(
-					camRef.current,
-					canvasRef.current,
-					lastVideoTime,
-					gestureRecognizer,
-					setLastVideoTime,
-					resCanvas
-				);
-			}
-		};
-
-		navigator.mediaDevices
-			.getUserMedia({ video: videoConstraints })
-			.then((stream) => {
-				if (camRef.current) {
-					camRef.current.srcObject = stream;
-					camRef.current.onloadedmetadata = () => {
-						camRef.current?.play();
-						startPrediction();
-					};
-				}
-			})
-			.catch((e) => {
-				console.log(e);
-			});
-	}, [gestureRecognizer, resCanvas]);
-
 	return (
 		<div className="relative min-w-[450px] flex-1 max-w-[450px] h-96 border-2 scale-x-[-1] border-gray-100">
 			<canvas
